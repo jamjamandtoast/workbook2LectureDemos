16a0817cb112fc0e4bab4b51974c86d98c04c950
Jams Jams <yarinyroblero@Jamyss-iMac.local>
2023-04-24 15:27:18 -0400

    first demo

---
+++ FirstCode.js
@@ -0,0 +1,37 @@
+"use strict";
+
+var firstName;
+
+firstName = "Jamys";
+
+var greeting;
+
+greeting = ("Your name is " + firstName);
+
+console.log(greeting);
+
+var cutstomerid;
+cutstomerid="88907";
+
+var customerNamefml;
+customerNamefml = "John Larry Doe";
+
+var gender;
+gender = "Female";
+
+var licensenumber;
+licensenumber = "5678936756";
+
+var policynumber;
+policynumber = "956767383";
+
+
+var firstName;
+
+firstName = "Jamys";
+
+var cityName;
+
+cityName; "West Palm Beach"
+
+var stateName = "Florida"
---
+++ var.js
@@ -0,0 +1,12 @@
+"use strict";
+
+var firstName;
+
+firstName = "Jamys";
+
+var cityName;
+
+cityName; "West Palm Beach"
+
+var stateName;
+stateName = "Florida"
